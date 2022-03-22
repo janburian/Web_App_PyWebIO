ff6afe95d09edb1ea3f9b23848900a812613eb13
janburian <jburian@students.zcu.cz>
2022-03-22 11:51:46 +0100

    Added methods

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from pywebio.input import input, FLOAT
-from pywebio.output import put_text
+from pywebio.input import *
+from pywebio.output import *
+import re
 
+def check_email(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if (re.fullmatch(regex, email)):
+        return
+    return "Bad email."
 
+def get_user_info():
+    info = input_group("User info",[
+      input('Input your name', name='name', required=True),
+      input('Input your email', name='email', type=TEXT, required=True, validate=check_email)
+    ])
+    put_text(info['name'], info['email'])
+    return info
 
+def upload_files():
+    imgs = file_upload("Upload .czi files:", accept=".czi", multiple=True)
+    for img in imgs:
+        put_image(img['content']) #TODO: save pictures
 
-
-
-
-if __name__ == '__main__':
+def choose_operation():
+    # predict, train
     pass
+
+def choose_model():
+    pass
+
+
+if __name__=="__main__":
+    user_info = get_user_info()
+    upload_files()
+
+    print()
+
+
+
+
+
