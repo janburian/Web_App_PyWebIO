ddce40a12bf4e24a2b2b1a7b35aa4f31864280a6
janburian <jburian@students.zcu.cz>
2022-03-28 11:15:22 +0200

    Added methods that create COCO dataset

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 import sys
+
+import pywebio.session
 import skimage.io
 
 from pywebio.input import *
@@ -8,10 +11,14 @@ import re
 
 from pathlib import Path
 
+import COCO_json
+
 path_to_script = Path("~/projects/scaffan/").expanduser()
 sys.path.insert(0, str(path_to_script))
 import scaffan.image
 import imma.image
+
+import json
 
 def check_email(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
@@ -36,9 +43,19 @@ def upload_data_page():
 
     return data
 
+def create_directory(directory_name):
+    current_directory = os.getcwd()
+    files_directory = os.path.join(current_directory, directory_name)
+    if not os.path.exists(files_directory):
+        os.makedirs(files_directory)
+    return files_directory
+
 def save_czi_files(images): # TODO: saving data to folder
+    czi_files_directory = create_directory("czi_files")
+
     for img in images:
-        open(img['filename'], 'wb').write(img['content'])
+        open(os.path.join(czi_files_directory, img['filename']), 'wb').write(img['content'])
+
 
 def get_czi_file_names(images):
     czi_file_names = []
@@ -49,14 +66,12 @@ def get_czi_file_names(images):
     return czi_file_names
 
 def czi_to_jpg(czi_files, czi_file_names):
-    path_images = Path(
-        r"C:\Users\janbu\OneDrive\Desktop"
-    )  # path to directory, where the images will be saved
+    images_directory = create_directory("images")
 
     index = 0
     while index < len(czi_files):
-        fn_str = (czi_file_names[index])
-        fn_path = Path(fn_str)
+        fn_path = Path(os.path.join(os.getcwd(), "czi_files", czi_file_names[index]))
+        fn_str = str(fn_path)
         if not fn_path.exists():
             break
         print(f"filename: {fn_path} {fn_path.exists()}")
@@ -67,9 +82,89 @@ def czi_to_jpg(czi_files, czi_file_names):
             pixelsize_mm=[0.0003, 0.0003]
         )  # wanted pixelsize in mm in view
         img = view.get_raster_image()
-        os.makedirs(path_images, exist_ok=True)
-        skimage.io.imsave(os.path.join(path_images, str(index).zfill(4) + ".jpg"), img)
+        skimage.io.imsave(os.path.join(images_directory, str(index).zfill(4) + ".jpg"), img)
         index += 1
+
+
+def create_COCO_json():
+    # Directory of the image dataset
+    dataset_directory = Path(os.path.join(os.getcwd(), "images"))
+
+    # Directory of the .czi files
+    czi_files_directory = Path(os.path.join(os.getcwd(), "czi_files"))  # path to .czi files directory
+
+    data = {}
+
+    """
+    Info
+    """
+    version = "1.0"
+    description = "COCO dataset for scaffan"
+    contributor = "Jan Burian" # TODO: chnage contributor (user info)
+
+    info_dictionary = COCO_json.get_info_dictionary(version, description, contributor)
+    data.update({"info": info_dictionary})
+
+    """
+    Images
+    """
+    list_image_dictionaries = COCO_json.get_image_properties(dataset_directory)
+    data.update({"images": list_image_dictionaries})
+
+    """
+    Categories
+    """
+    list_category_dictionaries = COCO_json.get_category_properties(
+        dataset_directory, "categories.txt"
+    )  # directory and .txt file
+    data.update({"categories": list_category_dictionaries})
+
+    """
+    Annotations
+    """
+    annotation_name = "annotation" # TODO: change of annotation names
+    pixelsize_mm = [0.0003, 0.0003]
+    list_annotation_dictionaries = COCO_json.get_annotations_properties(
+        czi_files_directory, annotation_name, pixelsize_mm
+    )
+    data.update({"annotations": list_annotation_dictionaries})
+
+    return data
+
+
+def copy_images():
+    source_dir = os.path.join(os.getcwd(), "images")
+    destination_dir = os.path.join(os.getcwd(), "COCO_dataset", "images")
+    shutil.copytree(source_dir, destination_dir)
+
+
+def create_COCO_dataset():
+    json_COCO = create_COCO_json()
+
+    COCO_directory = create_directory("COCO_dataset")
+
+    name_json = "trainval"
+
+    # Creating .json file
+    with open(os.path.join(COCO_directory, name_json), "w", encoding="utf-8") as f:
+        json.dump(json_COCO, f, ensure_ascii=False, indent=4)
+        f.close()
+
+    copy_images()
+
+
+def get_categories():
+    categories = input("Please, write category/categories separated by commas: ", required=True)
+    return categories
+
+
+def create_txt_categories_file(list_categories):
+    txt_file_path = os.path.join(os.getcwd(), "images", "categories.txt")
+    with open(txt_file_path, 'w') as f:
+        for category in list_categories:
+            f.write(category)
+            f.write("\n")
+    f.close()
 
 
 if __name__=="__main__":
@@ -92,6 +187,10 @@ if __name__=="__main__":
            open(own_model['filename'], 'wb').write(own_model['content'])
 
     elif (operation == 'Train'):
+        categories = get_categories().split(", ")
+        create_txt_categories_file(categories)
+
+        create_COCO_dataset()
         pass
 
 
