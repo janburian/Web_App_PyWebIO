813c7a6fd1d482f4100f85ce514f3de68d73d039
janburian <jburian@students.zcu.cz>
2022-04-05 12:03:14 +0200

    Added some tests

---
+++ tests/cv_test.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+import detectron2_testovaci
+from pathlib import Path
+
+def test_cv():
+    fn = Path(__file__).parent / "0000.jpg"
+    fn_out = Path(__file__).parent
+
+    detectron2_testovaci.predict((fn), (fn_out))
+
+    assert Path(fn_out / "output.jpg").exists() # testuje, zda "output.jpg" existuje
+
